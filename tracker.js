7b8396906e44a532126c6e7f95ff33f1bfac433d
Sankate Sharma <sankate@sankate.com>
2021-10-19 06:00:12 -0500

    Create tracker.js

---
+++ tracker.js
@@ -0,0 +1,17 @@
+<!-- Default Statcounter code for Sankate's Home Page
+http://www.sankate.com -->
+<script type="text/javascript">
+var sc_project=3047787; 
+var sc_invisible=1; 
+var sc_security="967d0daf"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics
+Made Easy - Statcounter" href="https://statcounter.com/"
+target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/3047787/0/967d0daf/1/"
+alt="Web Analytics Made Easy - Statcounter"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
